c67f61e133c606f6d51de12155e1a1dc4ef8fbcc
root <root@ip-172-31-88-176.ec2.internal>
2018-02-22 13:37:54 +0000

    TEST

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+echo "Hi there ... <br> This is a simple PHP application deployed by Capistrano <br><br>";
+echo "Today is " . date("Y-m-d");
+date_default_timezone_set("Africa/Nairobi");
+echo " and the time is " . date("h:i:sa") . "<br><br>";
+echo "I thought you should know also know that ";
+
+$date = strtotime("June 31, 2018 2:00 PM");
+$remaining = $date - time();
+$days_remaining = floor($remaining / 86400);
+$hours_remaining = floor(($remaining % 86400) / 3600);
+echo " there are $days_remaining days and $hours_remaining hours remaining to June 31st 2018"
+?>
+</body>
+</html>
